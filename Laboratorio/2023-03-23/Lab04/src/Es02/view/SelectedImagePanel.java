dd8d66b4147afac2a06fb3fdabb7e52d56fe720d
rhaegal222@gmail.com <rhaegal222@gmail.com>
2023-05-04 17:15:50 +0200

    Esercitazione del 23 Aprile Completata

--- Laboratorio/2023-03-23/Lab04/src/Es02/view/SelectedImagePanel.java
+++ Laboratorio/2023-03-23/Lab04/src/Es02/view/SelectedImagePanel.java
@@ -1,4 +1,21 @@
 package Es02.view;
+import java.awt.Graphics;
 
-public class SelectedImagePanel {
+import javax.swing.JPanel;
+
+public class SelectedImagePanel extends JPanel {
+    private ImageView img = null;
+    public void changeBackground(ImageView i){
+        img = i;
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponents(g);
+        if(img!=null){
+            g.drawImage(img.getImage(),0,0,this.getWidth(),this.getHeight(),null);
+        }
+    }
+
 }
